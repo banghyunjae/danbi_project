c9563b0736d1f935d68bd119e02ed223e3345ac7
banghyunjae <banghyunjae@naver.com>
2023-08-10 18:48:53 +0900

    update: url 추가/변경

--- danbi_project/urls.py
+++ danbi_project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("danbi.urls")),
+    path("danbi/", include("danbi.urls")),
+    path("users/", include("users.urls")),
 ]
