4f18a2196091d93b57532087ad0743c499757f6d
banghyunjae <banghyunjae@naver.com>
2023-08-11 13:41:45 +0900

    delete: user app 삭제

--- danbi_project/urls.py
+++ danbi_project/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("danbi/", include("danbi.urls")),
-    path("users/", include("users.urls")),
 ]
