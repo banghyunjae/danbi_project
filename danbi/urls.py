70ace237f243be3162296a50e4efb03cb3caf397
banghyunjae <banghyunjae@naver.com>
2023-08-11 13:39:43 +0900

    update: user 라우팅 추가

--- danbi/urls.py
+++ danbi/urls.py
@@ -4,10 +4,16 @@ from django.urls import path
 app_name = "danbi"
 
 urlpatterns = [
+    # Team
     path("teams/", views.TeamList.as_view(), name="team_list"),
     path("teams/<int:pk>/", views.TeamDetail.as_view(), name="team_detail"),
+    # User
+    path("users/", views.UserList.as_view(), name="user_list"),
+    path("users/<int:pk>/", views.UserList.as_view(), name="user_detail"),
+    # Task
     path("tasks/", views.TaskList.as_view(), name="task_list"),
     path("tasks/<int:pk>/", views.TaskDetail.as_view(), name="task_detail"),
+    # SubTask
     path("subtasks/", views.SubTaskList.as_view(), name="subtask_list"),
     path("subtasks/<int:pk>/", views.SubTaskDetail.as_view(), name="subtask_detail"),
 ]
