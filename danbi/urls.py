a01ebe7370dd1c28bfaf31e70e912d1f2cae4ba3
banghyunjae <banghyunjae@naver.com>
2023-08-13 20:05:40 +0900

    :art: users 앱 생성으로 변경

--- danbi/urls.py
+++ danbi/urls.py
@@ -7,9 +7,6 @@ urlpatterns = [
     # Team
     path("teams/", views.TeamList.as_view(), name="team_list"),
     path("teams/<int:pk>/", views.TeamDetail.as_view(), name="team_detail"),
-    # User
-    path("users/", views.UserList.as_view(), name="user_list"),
-    path("users/<int:pk>/", views.UserList.as_view(), name="user_detail"),
     # Task
     path("tasks/", views.TaskList.as_view(), name="task_list"),
     path("tasks/<int:pk>/", views.TaskDetail.as_view(), name="task_detail"),
