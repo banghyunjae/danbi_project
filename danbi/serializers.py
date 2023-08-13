a681ef020792a555e2805d9f341f39a7871e1a50
banghyunjae <banghyunjae@naver.com>
2023-08-13 20:04:39 +0900

    :art: users 앱 생성으로 serializer 수정

--- danbi/serializers.py
+++ danbi/serializers.py
@@ -1,16 +1,10 @@
 from rest_framework import serializers
-from .models import Team, User, Task, SubTask
+from .models import Team, Task, SubTask
 
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields = "__all__"
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
         fields = "__all__"
 
 
